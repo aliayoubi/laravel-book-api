bac2fdf8d7bcbed2ad5e5cd48e72b20b4c05141f
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-30 17:49:10 -0300

    review test

---
+++ tests/Unit/ReviewTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Review;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReviewTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_a_review_can_be_created()
+    {
+        $user = factory(App\User::class)->create();
+        $book = $user->books()->save(factory(App\Book::class)->make());
+
+        $review = $user->reviews()->create([
+            'title' => 'This book is so awesome!',
+            'description' => 'My only regret is not having read it before. Just read it!',
+            'note' => 10,
+            'book_id' => $book->id,
+        ]);
+
+        $latest_review = Review::latest()->first();
+
+        $this->assertEquals('This book is so awesome!', $latest_review->title);
+        $this->assertEquals('My only regret is not having read it before. Just read it!', $review->description);
+        $this->assertEquals(10, $review->note);
+
+        $this->seeInDatabase('reviews', ['id' => 1, 'title' => 'This book is so awesome!']);
+    }
+}
